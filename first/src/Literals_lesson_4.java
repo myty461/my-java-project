7ec631167499b7026527c72f67ca70ab5eed06e5
myty461 <73383190+myty461@users.noreply.github.com>
2023-01-19 08:34:21 -0800

    Add files via upload

---
+++ first/src/Literals_lesson_4.java
@@ -0,0 +1,20 @@
+//literals
+public class Literals_lesson_4 {
+    public static void main(String[] args) {
+        byte no1 = 120;
+        short no2 = 660;
+        int no3 = 65650;
+        long no4 = 2345675L;
+        String str = "hello world";
+
+        System.out.println(no1);
+        System.out.println(no2);
+        System.out.println(str);
+
+
+
+
+
+    }
+
+}
